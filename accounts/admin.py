d3e0256e7bb87333df5d5f77a15e6e709f11e929
ntran306 <nathangsu306@gmail.com>
2025-09-18 15:59:22 -0400

    updated security phrase

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from .models import SecurityQuestion, UserSecurityAnswer
+from .models import UserSecurityPhrase   # ← update this import
 
-@admin.register(SecurityQuestion)
-class SecurityQuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "text")
-    search_fields = ("text",)
-
-@admin.register(UserSecurityAnswer)
-class UserSecurityAnswerAdmin(admin.ModelAdmin):
-    list_display = ("user", "question", "updated_at")
-    search_fields = ("user__username", "question__text")
+@admin.register(UserSecurityPhrase)
+class UserSecurityPhraseAdmin(admin.ModelAdmin):
+    list_display = ("user", "updated_at")
+    search_fields = ("user__username", "user__email")
